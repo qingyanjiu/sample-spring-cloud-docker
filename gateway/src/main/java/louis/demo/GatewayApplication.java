151a9f44dfc7fe753a4b59f6ff15c0e13aebb8b3
louisliu <louisliu@cisco.com>
2019-03-21 17:22:14 +0800

    发现 archaius 和spring cloud config冲突，如果引用了config, archaius 就监听不到值的变化,所以想扩展PropertySourcesPlaceholderConfigurer来自己取配置，结果能取到配置，但eureka的很多placeholder没有写进去，于是继续研究中

--- gateway/src/main/java/louis/demo/GatewayApplication.java
+++ gateway/src/main/java/louis/demo/GatewayApplication.java
@@ -1,26 +1,16 @@
 package louis.demo;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import louis.demo.filters.PreFilter;
-
-import org.springframework.ui.ModelMap;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @Controller
 @EnableZuulProxy
 public class GatewayApplication {
-    @RequestMapping("/")
-    public String greeting(ModelMap map) {
-        String jreVersion = System.getProperty("java.specification.version");
-        map.addAttribute("jreVersion", "v" + jreVersion);
-        return "index";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
