d48acbd42f52af9504cbc479e03e003115769af2
louisliu <louisliu@cisco.com>
2019-06-10 13:01:30 +0800

    add

--- service-consumer/src/main/java/louis/demo/serviceconsumer/proxy/TestProxy.java
+++ service-consumer/src/main/java/louis/demo/serviceconsumer/proxy/TestProxy.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Proxy;
 public class TestProxy {
 
     public static void main(String[] args){
+        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
         IPrinter printer = new Printer();
         PrinterProxy handler = new PrinterProxy(printer);
         IPrinter proxy = (IPrinter) Proxy.newProxyInstance(printer.getClass().getClassLoader(),printer.getClass().getInterfaces(),handler);
